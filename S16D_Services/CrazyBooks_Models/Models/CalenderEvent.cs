7fedec62fd0a34c5b34ef9bc981c4700b1afb863
SachaPN <2184846@cegepmontpetit.ca>
2022-06-29 13:30:13 -0400

    FCT CalenderEvent
    CalenderEvent

---
+++ S16D_Services/CrazyBooks_Models/Models/CalenderEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyBooks_Models.Models
+{
+    public class CalenderEvent
+    {
+        int Id { get; set; }
+
+        DateTime Date { get; set; }
+
+        CategoryEvent CategoryEvent { get; set; }
+
+    }
+}
